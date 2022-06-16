d889bbffaf4eef7e3e8366407ffaa01c3006c8f2
Gergely Fehervari <fehergeri13@gmail.com>
2022-06-16 13:35:13 +0200

    v1.0.4

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,8 @@ export {JsonAwp} from "./JsonAwp"
 export {NumberString} from "./NumberString"
 export {OptionalNetlist} from "./OptionalNetlist"
 export {StaticJsonAwp} from "./StaticJsonAwp"
+
+export * from "./canvas/index"
+export * from "./boards/index"
+export * from "./options/index"
+export * from "./paths/index"
--- src/paths/index.ts
+++ src/paths/index.ts
@@ -1,1 +1,4 @@
 export {AwpPathCommon} from "./AwpPathCommon"
+export * from "./wire/index"
+export * from "./segments/index"
+export * from "./glue/index"
